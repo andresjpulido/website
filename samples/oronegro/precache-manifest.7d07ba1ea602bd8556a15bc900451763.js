a4ec49271c7bf8007d74e40f66684711fb2e346a
andresjpulido <andresjpulido@gmail.com>
2019-02-13 10:49:06 +1300

    add sample oronegro

---
+++ samples/oronegro/precache-manifest.7d07ba1ea602bd8556a15bc900451763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e624ef5f2e8191fa343",
+    "url": "/static/js/main.7e624ef5.chunk.js"
+  },
+  {
+    "revision": "e475ad7e31f3d73e175b",
+    "url": "/static/js/1.e475ad7e.chunk.js"
+  },
+  {
+    "revision": "7e624ef5f2e8191fa343",
+    "url": "/static/css/main.b3c95ead.chunk.css"
+  },
+  {
+    "revision": "25cc5ef5be1fdf7d7a993b0808c5e1de",
+    "url": "/index.html"
+  }
+];
