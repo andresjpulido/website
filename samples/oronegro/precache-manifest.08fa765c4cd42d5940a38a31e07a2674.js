6e4ffa02a27911e302ca7b8b37bc3c97aeb44892
andresjpulido <andresjpulido@gmail.com>
2019-02-14 09:03:38 +1300

    modificacion en la navegacion

---
+++ samples/oronegro/precache-manifest.08fa765c4cd42d5940a38a31e07a2674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2871535a34ead3ba0058",
+    "url": "/static/js/main.2871535a.chunk.js"
+  },
+  {
+    "revision": "0daf12446266e0a900ea",
+    "url": "/static/js/1.0daf1244.chunk.js"
+  },
+  {
+    "revision": "2871535a34ead3ba0058",
+    "url": "/static/css/main.b3c95ead.chunk.css"
+  },
+  {
+    "revision": "f73488998b78f102b0355a1cf99c8de3",
+    "url": "/index.html"
+  }
+];
