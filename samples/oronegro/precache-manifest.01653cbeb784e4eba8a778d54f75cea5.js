aaf4d1186cb3870b3a7b2e39447ca56608ea70b8
andresjpulido <andresjpulido@gmail.com>
2019-02-14 09:42:05 +1300

    nueva version

---
+++ samples/oronegro/precache-manifest.01653cbeb784e4eba8a778d54f75cea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5275386f7eb34ecc50fc",
+    "url": "/samples/oronegro/static/js/runtime~main.5275386f.js"
+  },
+  {
+    "revision": "a6012a225cd1a0cd7d5b",
+    "url": "/samples/oronegro/static/js/main.a6012a22.chunk.js"
+  },
+  {
+    "revision": "b09ba838416c04018571",
+    "url": "/samples/oronegro/static/js/1.b09ba838.chunk.js"
+  },
+  {
+    "revision": "a6012a225cd1a0cd7d5b",
+    "url": "/samples/oronegro/static/css/main.8b025093.chunk.css"
+  },
+  {
+    "revision": "6f19112f2ed72e6f3342b81169692d4f",
+    "url": "/samples/oronegro/index.html"
+  }
+];
--- samples/oronegro/service-worker.js
+++ samples/oronegro/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/oronegro/precache-manifest.8b130ba7fda5b620be66453773c67b95.js"
+  "/samples/oronegro/precache-manifest.01653cbeb784e4eba8a778d54f75cea5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/oronegro/index.html", {
+workbox.routing.registerNavigationRoute("/samples/oronegro/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
