3e69a2471d47dd1cb5ca790ad25bd0f42ede9cb3
Andres <andresjpulido@gmail.com>
2020-12-06 13:46:37 +1300

    Updates

---
+++ precache-manifest.8a09ffdfc946c7386afa422c08b4af5c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed8a53e8e77766949d065feb2b9e8bc",
+    "url": "/andresjpulido.github.io/index.html"
+  },
+  {
+    "revision": "ba41b1a3a00fc27855c1",
+    "url": "/andresjpulido.github.io/static/css/main.fcf808cc.chunk.css"
+  },
+  {
+    "revision": "96271e8633a0f2268827",
+    "url": "/andresjpulido.github.io/static/js/2.b7d572b8.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/andresjpulido.github.io/static/js/2.b7d572b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba41b1a3a00fc27855c1",
+    "url": "/andresjpulido.github.io/static/js/main.5e8fec2d.chunk.js"
+  },
+  {
+    "revision": "8d1251a3a79ad5e170aa",
+    "url": "/andresjpulido.github.io/static/js/runtime-main.39cf41af.js"
+  },
+  {
+    "revision": "387af7407a78d37acb80c89546063203",
+    "url": "/andresjpulido.github.io/static/media/background-white.387af740.png"
+  },
+  {
+    "revision": "c0fa4450190363e0239c72513510db92",
+    "url": "/andresjpulido.github.io/static/media/background.c0fa4450.png"
+  },
+  {
+    "revision": "1d9a88c67fd8262a63d440f2735e9b78",
+    "url": "/andresjpulido.github.io/static/media/timeline1.1d9a88c6.png"
+  },
+  {
+    "revision": "c65fe7b55f6473601cd70f24656d07b9",
+    "url": "/andresjpulido.github.io/static/media/verana-bold-webfont.c65fe7b5.woff2"
+  }
+]);
