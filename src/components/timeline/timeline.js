0036c5e1087b86c02aaa6cb56d176f003cbddfae
Andres <andresjpulido@gmail.com>
2021-02-22 21:04:01 +1300

    change the swiper

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import wexperience from '../../data/wexperience.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './timeline.scss'
+ 
 
 export default function Timeline() {
 
-  
+
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
- 
+
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentPageIndex(currentPageIndex => {
                 let nprojects = wexperience.length;
-                if(currentPageIndex === nprojects-1)
+                if (currentPageIndex === nprojects - 1)
                     return 0;
                 else
                     return currentPageIndex + 1
@@ -28,67 +29,67 @@ export default function Timeline() {
     let currentyear = wexperience[currentPageIndex].year;
     let curretyearposition = 0;
 
-    for (var i=0;i<years.length;i++){
-        if(years[i]===currentyear){
-            curretyearposition=i
+    for (var i = 0; i < years.length; i++) {
+        if (years[i] === currentyear) {
+            curretyearposition = i
             break;
-        }         
+        }
     }
 
-    var index = Math.floor(curretyearposition /size);
-    years = years.slice((index*5), ((index*5)+5))
-    
-    return (
-     
-    <div className="timeline">
-        <div className="carousel">
-            
-            <ul className="page">
-                {
+    var index = Math.floor(curretyearposition / size);
+    years = years.slice((index * 5), ((index * 5) + 5))
 
-                   <li>
-                        <h1>{wexperience[currentPageIndex].name} - {wexperience[currentPageIndex].company}</h1>
-                        <p>{wexperience[currentPageIndex].description}</p>
-                        <p className="toolsets">Toolsets: 
+    return (
+
+        <div className="timeline">
+            <div className="carousel">
+
+                <ul className="page">
+                    {
+
+                        <li>
+                            <h1>{wexperience[currentPageIndex].name} - {wexperience[currentPageIndex].company}</h1>
+                            <p>{wexperience[currentPageIndex].description}</p>
+                            <p className="toolsets">Toolsets:
                         {
-                            wexperience[currentPageIndex].technologies.map((tech, index) => <span className="tech" key={index}>  {tech}  | </span>)
-                        }    
-                        </p> 
-                    </li> 
-                }
-            </ul>
-            
+                                    wexperience[currentPageIndex].technologies.map((tech, index) => <span className="tech" key={index}>  {tech}  | </span>)
+                                }
+                            </p>
+                        </li>
+                    }
+                </ul>
+
+            </div>
+            <div className="swiper">
+                <ul className="swiperd">
+                    <li>
+                        <a href="#" onClick={(e) => { e.preventDefault(); }}>
+                            <img id="first" />
+                        </a>
+                    </li>
+                    {
+                        years.map((year, index) => <li key={index}>
+                            {
+                                wexperience[currentPageIndex].year === year ?
+                                    <a href="#" className="selected" onClick={(e) => { e.preventDefault(); }}>{year}</a>
+                                    :
+                                    <a href="#" onClick={(e) => { e.preventDefault(); }}>{year}</a>
+                            }
+
+                        </li>)
+                    }
+                    <li>
+                        <a href="#" onClick={(e) => { e.preventDefault(); }}>
+                            <img id="last" />
+                        </a>
+                    </li>
+                </ul>
+
+
+            </div>
+
         </div>
-        <div className="swiper">
-            <a href="#" onClick={(e)=>{e.preventDefault();}}><FontAwesomeIcon
-                className="icon-active"
-                icon={["fas", "caret-up"]}
-                key={2}
-                size="lg" />
-            </a>
-            <ul className="swiperd">
-                {
-                    years.map((year, index) => <li key={index}>
-                        {
-                            wexperience[currentPageIndex].year === year ?
-                            <a href="#" className="selected" >{year}</a>
-                            :
-                            <a href="#" >{year}</a>
-                        }
-                        
-                    </li>)
-                }
-            </ul>
-            <a href="#" onClick={(e)=>{e.preventDefault();}}><FontAwesomeIcon
-                className="icon-active"
-                icon={["fas", "caret-down"]}
-                key={2}
-                size="lg" />
-            </a>
-        </div>
-        
-    </div>
-    
-    
+
+
     )
 }
