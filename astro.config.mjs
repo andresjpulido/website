65b1a202b07212f5fe38b62a4f82a6ca8ace2684
Andres <andresjpulido@gmail.com>
2024-06-17 08:34:45 +1200

    adding complete url for main image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false })],
   site: 'https://andresjpulido.github.io',
-  base: '/',
+  base: '/' ,
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
