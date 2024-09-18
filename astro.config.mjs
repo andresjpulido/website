c26e15213f6b0f93d5af041f4472fc7b770ebaed
Andres <andresjpulido@gmail.com>
2024-09-18 23:10:33 +1200

    updating sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
-  site: 'https://andresjpulido.github.io',
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    sitemap()
+  ],
+  site: 'https://andresjpulido.com',
   base: '/' ,
   vite: {
     plugins: [basicSsl()],
